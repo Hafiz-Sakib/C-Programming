38d617ec872d3d35c92f45a508531fa376f817b8
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-08-25 10:18:36 +0600

    new

--- Nested Loops/Jenny/pattern5.c
+++ Nested Loops/Jenny/pattern5.c
@@ -1,8 +1,35 @@
+/* for input 5 ,output will be :
+
+    *
+   **
+  ***
+ ****
+*****
+
+*/
+
 #include <stdio.h>
 int main()
 {
 
-    int i, j, k, n;
-    printf("bruuuuuuuuuuuuuuuu");
+    int i, j, rows, n;
+    printf("Enter the number of rows\n");
+    scanf("%d", &rows);
+    for (i = 1; i <= rows; i++)
+    {
+        for (j = 1; j <= rows; j++)
+        {
+            if ((i + j) <= rows)
+            {
+                printf(" ");
+            }
+            else
+            {
+                printf("*");
+            }
+        }
+        printf("\n");
+    }
+
     return 0;
 }
