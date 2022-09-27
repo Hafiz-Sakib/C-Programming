f1225258ffb3c2c99802f5132fd7a5d2849d7867
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-09-27 19:57:42 +0600

    2D Array Pronting Structures Added

--- Array/2D Array/PrintA2DArray.c
+++ Array/2D Array/PrintA2DArray.c
@@ -8,6 +8,19 @@ int main()
         {5, 6, 7, 8},
         {9, 10, 11, 12}};
 
+    /*
+    Basic Structure
+
+    for (i = 0; i < row_no; i++)
+        {
+
+            for (j = 0; j < col_no; j++)
+            {
+                printf("%d", A[i][j]);
+            }
+        }
+ */
+
     for (i = 0; i < 3; i++)
     {
         for (j = 0; j < 4; j++)
