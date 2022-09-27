2c8386bab40bf5c6e4f013f602466cdd5663feab
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-09-27 15:21:44 +0600

    name print using array

--- Array/Anisul/2.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int numbers[] = {1, 2, 3, 4, 5};
-
-    return 0;
-}
---
+++ Array/Practise Problems of Array/NamePrintWithArray.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    int n;
+    printf("How Many Characters in your name?\n");
+    scanf("%d", &n);
+    char name[n];
+    int i;
+    printf("Enter The Characters One By One :\n");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%s", &name[i]);
+    }
+
+    printf("Your name is :%s", name);
+
+    return 0;
+}
