b90d84f163c1aacd34eb61f0eed2b5602098e0b7
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-10-03 23:24:32 +0600

    Code Screenshots added

--- Array/Basic 1D Arrays/CodeForWin Practise Problems/SortingArrayInDescendingOrder.c
+++ Array/Basic 1D Arrays/CodeForWin Practise Problems/SortingArrayInDescendingOrder.c
@@ -24,7 +24,7 @@ int main()
     {
         for (j = i + 1; j < size; j++)
         {
-            if (array[i] > array[j])
+            if (array[i] < array[j])
             {
                 temp = array[i];
                 array[i] = array[j];
