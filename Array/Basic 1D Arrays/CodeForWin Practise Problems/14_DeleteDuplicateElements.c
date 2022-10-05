7db681d16f9189f66ca752a25fa783fe86c4a038
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-10-05 13:58:51 +0600

    update

--- Array/Basic 1D Arrays/CodeForWin Practise Problems/14_DeleteDuplicateElements.c
+++ Array/Basic 1D Arrays/CodeForWin Practise Problems/14_DeleteDuplicateElements.c
@@ -1,48 +1,52 @@
-
-// Simple C program to remove duplicates
 #include <stdio.h>
+int IncrementalSort(int arr[], int size);
 
-// Function to remove duplicate elements This function
-// returns new size of modified array.
-int removeDuplicates(int arr[], int n)
-{
-    // Return, if array is empty or contains a single
-    // element
-    if (n == 0 || n == 1)
-        return n;
-
-    int temp[n];
-
-    // Start traversing elements
-    int j = 0;
-    // If current element is not equal to next element
-    // then store that current element
-    for (int i = 0; i < n - 1; i++)
-        if (arr[i] != arr[i + 1])
-            temp[j++] = arr[i];
-
-    // Store the last element as whether it is unique or
-    // repeated, it hasn't stored previously
-    temp[j++] = arr[n - 1];
-
-    // Modify original array
-    for (int i = 0; i < j; i++)
-        arr[i] = temp[i];
-
-    return j;
-}
-
-// Driver code
 int main()
 {
-    int arr[] = {1, 2, 2, 3, 4, 4, 4, 5, 5};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int size, MAX = 1000, i, j, temp;
+    int array[MAX];
 
-    // removeDuplicates() returns new size of array.
-    n = removeDuplicates(arr, n);
+    printf("Enter The Array Size :\n");
+    scanf("%d", &size);
 
-    // Print updated array
-    for (int i = 0; i < n; i++)
-        printf("%d ", arr[i]);
+    for (i = 0; i < size; i++)
+    {
+        printf("Enter %d no number :\n", (i + 1));
+        scanf("%d", &array[i]);
+    }
+    printf("Numbers of Your Array are:\n");
+    for (i = 0; i < size; i++)
+    {
+        printf("%d ", array[i]);
+    }
+
+    // Calling the Function for Sort
+
+    array[size] = IncrementalSort(array, size);
+
+    printf("\nUniqe Elements are:\n");
+    for (i = 0; i < size; i++)
+    {
+        printf("%d ", array[i]);
+    }
+
     return 0;
+}
+
+int IncrementalSort(int arr[], int size)
+{
+    int i, j, temp;
+    for (i = 0; i < size; i++)
+    {
+        for (j = i + 1; j < size; j++)
+        {
+            if (arr[i] > arr[j])
+            {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return arr[size];
 }
