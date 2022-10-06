17c1b833dc4e942575fdcbd94b29232985f29d90
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-10-06 20:28:17 +0600

    Search a Specific Element In Array

--- Array/Basic 1D Arrays/CodeForWin Practise Problems/18__SearchAElementInArray.c
+++ Array/Basic 1D Arrays/CodeForWin Practise Problems/18__SearchAElementInArray.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
 int main()
 {
+    int size, i, element, found = -1;
+
+    printf("Enter The Array Size :\n");
+    scanf("%d", &size);
+    int array[size];
+
+    for (i = 0; i < size; i++)
+    {
+        printf("Enter %d no number :\n", (i + 1));
+        scanf("%d", &array[i]);
+    }
+    printf("Numbers of Your Array are:\n");
+    for (i = 0; i < size; i++)
+    {
+        printf("%d ", array[i]);
+    }
+
+    printf("\nEnter the Element you want to find in the array :\n");
+    scanf("%d", &element);
+
+    for (i = 0; i < size; i++)
+    {
+        if (array[i] == element)
+        {
+            found = i;
+            break;
+        }
+    }
+
+    if (found != -1)
+    {
+        printf("Your Searched element %d is Found and It locates in %d position!", element, (found + 1));
+    }
+    else
+    {
+        printf("Sorry! Your Searched element %d is not found!\n", element);
+    }
 
     return 0;
 }
