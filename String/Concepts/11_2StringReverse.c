a3fcb74bfdaec94c046a759152d8d45e58508184
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-10-11 00:58:19 +0600

    reverse string without library function

--- String/Concepts/11_2StringReverse.c
+++ String/Concepts/11_2StringReverse.c
@@ -6,5 +6,15 @@ int main()
     char string1[100], string2[100];
     printf("Enter String 1 : ");
     gets(string1);
+    int len = strlen(string1), j = 0, i;
+
+    for (i = (len - 1); i >= 0; i--)
+    {
+        string2[j] = string1[i];
+        j++;
+    }
+
+    printf("%s", string2);
+
     return 0;
 }
