fcc7ec2badb7642455112c87105f8c44b508d2f0
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-10-10 13:32:16 +0600

    change

---
+++ String/Concepts/practise.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+
+    char String1[100], String2[100];
+    printf("Enter String 1 : ");
+    gets(String1);
+    printf("Enter String 2 : ");
+    gets(String2);
+
+    return 0;
+}
