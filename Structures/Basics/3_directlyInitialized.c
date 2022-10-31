5c86418c155675630cf5aa9606ab2a1fcb315ee1
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-11-01 00:36:26 +0600

    Structures direct initialize

--- Structures/Basics/3_directlyInitialized.c
+++ Structures/Basics/3_directlyInitialized.c
@@ -8,9 +8,11 @@ struct person
 
 int main()
 {
+    // element wise assignment way 01 :
+
     struct person sakib = {21, 21500.45};
 
-    // element wise assignment  :
+    // element wise assignment way 02 :
 
     struct person ohid, yousha;
 
@@ -19,8 +21,14 @@ int main()
     ohid.salary = 2222.22;
     yousha.salary = 21511.22;
 
-    printf("Age of  Sakib : %d\n", sakib.age);
-    printf("Salary of  Sakib : %.2f\n", sakib.salary);
+    // structure Variable Assignment
+
+    struct person nawaz;
+
+    nawaz = yousha; // Yousha এর ইনফরমেশনগুলো নওয়াজ ভেরিয়েবল এর মধ্যে চলে আসবে।এককথায়,এক স্ট্রাকচার ভেরিয়েবল থেকে অন্য স্ট্রাকচার ভেরিয়েবল এর মধ্যে কপি করা
+
+    printf("Age of Nawaz : %d\n", nawaz.age);
+    printf("Salary of  Nawaz : %.2f\n", nawaz.salary);
 
     return 0;
 }
