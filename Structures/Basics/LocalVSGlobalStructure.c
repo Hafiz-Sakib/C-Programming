7196c827fe2304444dc0edde9afb8aa66441a648
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-11-01 00:16:11 +0600

    Structures in C Programming

---
+++ Structures/Basics/LocalVSGlobalStructure.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+struct person sakib, ohid; // Global Variable
+int main()
+{
+    // Local Structure
+
+    // Global Structure can be accessed from any function but  local function can be accessed only in main function
+
+    struct person
+    {
+        int age;
+        float salary;
+    };
+
+    struct person sakib, ohid; // Local Variable
+
+    sakib.age = 21;
+    ohid.age = 22;
+    sakib.salary = 20200.250;
+    ohid.salary = 202000.250;
+
+    printf("Age of  Sakib : %d\n", sakib.age);
+    printf("Age of  Ohid : %d\n", ohid.age);
+    printf("Salary of  Sakib : %.2f\n", sakib.salary);
+    printf("Salary of  Ohid : %.2f\n", ohid.salary);
+
+    return 0;
+}
