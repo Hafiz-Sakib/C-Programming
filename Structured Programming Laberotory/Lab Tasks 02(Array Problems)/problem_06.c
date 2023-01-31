737e96eb744815828202b65b8b1e10a635e8c0a4
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-01-31 23:28:02 +0600

    Problem Solved

---
+++ Structured Programming Laberotory/Lab Tasks 02(Array Problems)/problem_06.c
@@ -0,0 +1,35 @@
+/*
+Name : Hafiz-Sakib
+Id : 0222210005101118
+ */
+
+// Write a C program that read n number of values and prints the maxSumimum sum of 2 values.
+
+#include <stdio.h>
+int main()
+{
+    int n, currentSumSum = 0, maxSum = 0;
+    scanf("%d", &n);
+    int a[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            int currentSum = a[i] + a[j];
+            if (currentSum > maxSum)
+            {
+                maxSum = currentSum;
+            }
+        }
+    }
+
+    printf("%d\n", maxSum);
+
+    return 0;
+}
--- Structured Programming Laberotory/Lab Tasks 02(Array Problems)/problem__05.c
+++ Structured Programming Laberotory/Lab Tasks 02(Array Problems)/problem__05.c
@@ -3,7 +3,7 @@ Name : Hafiz-Sakib
 Id : 0222210005101118
  */
 
-// Write a C program that read n number of values and prints the maximum consecutive currentSum of 2 values.
+// Problem 05 : Write a C program that read n number of values and prints the maximum consecutive currentSum of 2 values.
 
 #include <stdio.h>
 int main()
